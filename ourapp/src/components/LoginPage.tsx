c59dc9289e714122304bcedcf7444a09ba242f0b
Guilherme Sampaio <kaisergui258@gmail.com>
2021-03-13 08:07:33 -0300

    Criando parte funcional da LoginPage

--- ourapp/src/components/LoginPage.tsx
+++ ourapp/src/components/LoginPage.tsx
@@ -1,16 +1,37 @@
+import React from 'react';
 import styles from '../styles/components/LoginPage.module.css';
 
 export function LoginPage() {
 
+  let user = {};
+
+  function handleInput(event) {
+    this.user = {...this.user, [event.target.name]: event.target.value}
+  }
+
+  function login() {
+    fetch('http://localhost:3300/api/login',
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(this.user)
+      }
+    ).then(async res => {
+      let verify = 'true' == await res.json();
+      verify && (window.location.href='http://' + window.location.host + '/Home')
+    });
+  }
+ 
   return (
     <div className={styles.container}>
       <section>      
         <div>
           <label htmlFor="input">Login</label>
-          <input type="text" placeholder='Nome de usuário' />
+          <input name='login' type="text" placeholder='Nome de usuário' onChange={(event) => this.handleInput(event)} />
           <label htmlFor="input">Senha</label>
-          <input type="password" placeholder='Senha'/>
-          <button type='button'>Fazer Login</button>
+          <input name='senha' type="password" placeholder='Senha' onChange={(event) => this.handleInput(event)} />
+          <button type='button' onClick={this.login()}>Fazer Login</button>
         </div>
         <div>
           <img src="./login.png" alt="Icone de usuário"/>
